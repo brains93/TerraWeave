97ef2878f65f6db7d07f4f28047cc785df6e6423
Brains <grantc0493@gmail.com>
2025-05-09 11:01:47 +0100

    fixed init file

--- TerraWeave/TerraWeave.py
+++ TerraWeave/TerraWeave.py
@@ -1,6 +1,5 @@
 import subprocess
 import logging
-import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TerraWeave",  # Package name
+    name="terraweave",  # Package name
     version="0.1.1",  # Initial version
     description="A Python wrapper for Terraform",
     long_description=open("README.md").read(),
